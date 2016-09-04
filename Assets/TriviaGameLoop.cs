3b1ef34d8277cf68e41b463f30f7f93f5c2133d7
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-09-04 18:37:04 +0700

    Adding answer

--- Assets/TriviaGameLoop.cs
+++ Assets/TriviaGameLoop.cs
@@ -5,14 +5,16 @@ public class TriviaGameLoop : MonoBehaviour {
 
 	public struct Question{
 		public string questionText;
-		public Question(string questionText){
+		public string[] answers;
+		public Question(string questionText, string[] answers){
 			this.questionText = questionText;
+			this.answers = answers;
 		}
 	}
-	public Question testQuestion = new Question("What's your favourite color?");	
+	public Question testQuestion = new Question("What's your favourite color?",new string[]{"blue","red","yellow"});	
 	// Use this for initialization
 	void Start () {
-		print (testQuestion.questionText);
+		print (testQuestion.questionText+" "+testQuestion.answers[0]);
 	}
 	
 	// Update is called once per frame
