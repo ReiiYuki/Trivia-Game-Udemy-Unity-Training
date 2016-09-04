990ff09239041ab0d703da54b4fa4aae2d207778
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-09-04 18:31:40 +0700

    Test some question

--- Assets/TriviaGameLoop.cs
+++ Assets/TriviaGameLoop.cs
@@ -5,11 +5,14 @@ public class TriviaGameLoop : MonoBehaviour {
 
 	public struct Question{
 		public string questionText;
+		public Question(string questionText){
+			this.questionText = questionText;
+		}
 	}
-	public Question testQuestion;	
+	public Question testQuestion = new Question("What's your favourite color?");	
 	// Use this for initialization
 	void Start () {
-
+		print (testQuestion.questionText);
 	}
 	
 	// Update is called once per frame
