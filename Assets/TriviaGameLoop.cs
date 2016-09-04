032db9588ff7a6455259b9902da2ae9b717260a8
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-09-04 21:03:05 +0700

    Random Question

--- Assets/TriviaGameLoop.cs
+++ Assets/TriviaGameLoop.cs
@@ -13,12 +13,29 @@ public class TriviaGameLoop : MonoBehaviour {
 			this.correctAnswerIndex = correctAnswerIndex;
 		}
 	}
-	public Question currentQuestion = new Question("What's your favourite color?",new string[]{"blue","red","yellow","white","black"},0);	
+	private Question currentQuestion;	
 	public Button[] answerButtons;
 	public Text questionText;
+
+	private Question[] questions = new Question[10];
+	private int currentQuestionIndex;
 	// Use this for initialization
 	void Start () {
+
+		questions [0] = new Question ("a", new string[]{"お","た","あ","ろ","の" },2);
+		questions [1] = new Question ("も", new string[]{"chi","tsu","ha","ma","mo" },4);
+		questions [2] = new Question ("wa", new string[]{"ね","れ","わ","か","ら" },2);
+		questions [3] = new Question ("し", new string[]{ "chi","shi","mo","ho","no"},1);
+		questions [4] = new Question ("ne", new string[]{"へ","ひ","め","ね","わ" },3);
+		questions [5] = new Question ("ま", new string[]{"ma","chi","mo","to","ke" },0);
+		questions [6] = new Question ("mi", new string[]{"ま","む","り","み","は" },3);
+		questions [7] = new Question ("に", new string[]{"te","yu","re","ni","wa" },3);
+		questions [8] = new Question ("ri", new string[]{"ね","ふ","よ","り","ろ" },3);
+		questions [9] = new Question ("ぬ", new string[]{ "nu","me","ha","ni","ko"},0);
+		ChooseQuestions ();
+		currentQuestion = questions [currentQuestionIndex];
 		AssignQuestion ();
+
 	}
 
 	// Update is called once per frame
@@ -39,5 +56,9 @@ public class TriviaGameLoop : MonoBehaviour {
 		} else {
 			print ("incorrect");
 		}
+	}
+
+	void ChooseQuestions(){
+		currentQuestionIndex = Random.Range (0, questions.Length);
 	}
 }
