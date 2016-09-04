c600869cff1c1d91c26627587b4c90ddb0beb79d
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-09-04 19:03:56 +0700

    Adding text to ui

--- Assets/TriviaGameLoop.cs
+++ Assets/TriviaGameLoop.cs
@@ -1,24 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.UI;
 public class TriviaGameLoop : MonoBehaviour {
 
 	public struct Question{
 		public string questionText;
 		public string[] answers;
-		public int correctAnswer;
-		public Question(string questionText, string[] answers,int correctAnswer){
+		public int correctAnswerIndex;
+		public Question(string questionText, string[] answers,int correctAnswerIndex){
 			this.questionText = questionText;
 			this.answers = answers;
-			this.correctAnswer = correctAnswer;
+			this.correctAnswerIndex = correctAnswerIndex;
 		}
 	}
-	public Question testQuestion = new Question("What's your favourite color?",new string[]{"blue","red","yellow"},0);	
+	public Question currentQuestion = new Question("What's your favourite color?",new string[]{"blue","red","yellow","white","black"},0);	
+	public Button[] answerButtons;
+	public Text questionText;
 	// Use this for initialization
 	void Start () {
-		print (testQuestion.questionText+" "+testQuestion.answers[0]+" "+testQuestion.correctAnswer);
+		AssignQuestion ();
 	}
-	
+
+	void AssignQuestion(){
+		questionText.text = currentQuestion.questionText;
+		for (int i = 0; i<answerButtons.Length; i++) {
+			answerButtons [i].GetComponentInChildren<Text>().text = currentQuestion.answers [i];
+		}
+	}
+
 	// Update is called once per frame
 	void Update () {
 	
